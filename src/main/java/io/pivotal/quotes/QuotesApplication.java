55944264e372071a11722ae04297ec77b6a422be
Simon Rowe <srowe@pivotal.io>
2018-11-21 13:17:14 +0000

    changes to run on cloud

--- src/main/java/io/pivotal/quotes/QuotesApplication.java
+++ src/main/java/io/pivotal/quotes/QuotesApplication.java
@@ -5,6 +5,9 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.HttpsURLConnection;
+
 @SpringBootApplication
 @EnableDiscoveryClient
 @EnableCircuitBreaker
@@ -12,6 +15,11 @@ public class QuotesApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(QuotesApplication.class, args);
+    }
+
+    static {
+        HostnameVerifier allHostsValid = (name, sslSession) -> true;
+        HttpsURLConnection.setDefaultHostnameVerifier(allHostsValid);
     }
 }
 
